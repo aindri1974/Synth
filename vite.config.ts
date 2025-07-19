d3c9b31166c027eb57703f3bad28d5d44421a6a6
Aindri Singh <aindrisingh1974@gmail.com>
2025-07-19 11:20:00 +0530

    Added more features and debugged errors

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  
+  server: {
+
+    port: 3000,
+    strictPort: true,
+    headers: {
+      
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
